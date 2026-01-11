ae1286bf039e64b8502b2f16c2de257508dc6b4d
abdala hamwi <abdalah09941@gmail.com>
2026-01-11 15:29:50 +0300

    fix bug models.py

--- product/apps.py
+++ product/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EcommerceApiConfig(AppConfig):
+class ProductConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ecommerce_api'
+    name = 'product'
--- project/settings.py
+++ project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # my apps 
     'rest_framework',
-    'ecommerce_api',
+    'product',
     
 ]
 
