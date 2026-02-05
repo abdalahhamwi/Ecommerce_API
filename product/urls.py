7169297695b691858e4139e8c906a7e25576bd48
abdala hamwi <abdalah09941@gmail.com>
2026-02-05 15:24:51 +0300

    add  django_filters to requirements

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import list_products
+from . import views
 
 urlpatterns = [
-    path("products/", view=list_products, name="list_products"),
+    path("products/", views.list_products, name="list_products"),
+    path("products/<int:id>/", views.get_by_id_product, name="get_by_id_product"),
 ]
--- product/views.py
+++ product/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from product.models import Product
+from .models import Product
 from .serializers import ProductSerializer
-
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 
@@ -11,5 +11,11 @@ from .serializers import ProductSerializer
 def list_products(request):
     products = Product.objects.all()
     serializers = ProductSerializer(products, many=True)
-    print(products)
     return Response({"products": serializers.data})
+
+
+@api_view(["GET"])
+def get_by_id_product(request, id):
+    products = get_object_or_404(Product, id=id)
+    serializers = ProductSerializer(products, many=False)
+    return Response({"product": serializers.data})
