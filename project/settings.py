d29f899148a6c503f87bfe1f1194f61e8a844d0d
abdala hamwi <abdalah09941@gmail.com>
2026-02-27 21:12:38 +0300

    add custom JSON handlers for 404 and 500 responses

--- project/settings.py
+++ project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-+d-r-!rx0@8&@=s9b-31aq4_u*wjvv9c5xs%jqug4x+=glc5u-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- project/urls.py
+++ project/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('product.urls'))
 ]
+handler404 = 'project.utils.handler404'
+handler500 = 'project.utils.handler500'
