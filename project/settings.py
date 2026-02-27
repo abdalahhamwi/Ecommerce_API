7c9c1797d831c4099d3faffc173d71f310d35513
abdala hamwi <abdalah09941@gmail.com>
2026-02-27 22:13:45 +0300

    create accounts app and configure JWT authentication

--- project/settings.py
+++ project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "django_filters",
     "product",
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
@@ -84,6 +86,24 @@ DATABASES = {
     }
 }
 
+# REST_FRAMEWORK SETTINGS 
+REST_FRAMEWORK = {
+    
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+    
+}
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=15),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'BLACKLIST_AFTER_ROTATION': True,
+    'Auth_HEADER_TYPES': ('Bearer',),
+    'Auth_token_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    
+    
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
